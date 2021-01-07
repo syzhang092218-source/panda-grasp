c17084cf493257ce8c6e696ebafc9a1f31aae0ea
syzhang <syzhang092218@gmail.com>
2021-01-07 23:17:01 +0800

    install bug fixed

--- panda_grasp/__init__.py
+++ panda_grasp/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='PandaMoveBox-v0',
-    entry_point='panda_move_box.env:PandaMoveBoxEnv',
+    entry_point='panda_grasp.env:PandaMoveBoxEnv',
     max_episode_steps=10000,
 )
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-      name='panda_move_box',
+      name='panda_grasp',
       version='0.0.1',
+      packages=find_packages(),
       install_requires=['gym']  # And any other dependencies foo needs
 )
