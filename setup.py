96f4e28d2e38c187206e9a34f11c023228f03e00
syzhang <syzhang092218@gmail.com>
2021-04-27 16:02:37 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author='Songyuan Zhang, Tong Xiao',
     author_email='syzhang092218@gmail.com',
     packages=find_packages(),
-    install_requires=['numpy~=1.16.0', 'gym>=0.17.2', 'setuptools>=49.2.0', 'pygame>=2.0.1', 'pybullet>=2.8.4',
-                      'tqdm>=4.47.0', 'torch~=1.6.0', ]  # And any other dependencies foo needs
+    install_requires=['numpy>=1.16.0', 'gym>=0.17.2', 'setuptools>=49.2.0', 'pygame>=2.0.1', 'pybullet>=2.8.4',
+                      'tqdm>=4.47.0', 'torch>=1.6.0', ]  # And any other dependencies foo needs
 )
